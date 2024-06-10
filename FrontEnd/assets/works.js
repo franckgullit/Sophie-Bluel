a012a0bdd58c378e577a71a27a84229bfaf7d25e
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-10 16:32:01 +0200

    Create works.js
    First commit after recuperation of data and replacement of html figure elements with js content

---
+++ FrontEnd/assets/works.js
@@ -0,0 +1,33 @@
+//recuperation des donnees des travaux via l'API
+fetch("http://localhost:5678/api/works",)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let gallery = document.querySelector(".gallery");
+        data.forEach(
+            item => {
+                let figureElement = document.createElement("figure");
+                
+                //creating image element
+                const imgElement = document.createElement("img");
+                imgElement.src = item.imageUrl;
+
+                //adding created image to figure element
+                figureElement.appendChild(imgElement);
+
+                //creating caption
+                const figcaptionElement = document.createElement("h2");
+                figcaptionElement.innerText = item.title;
+
+                 //adding caption to figure
+                 figureElement.appendChild(figcaptionElement)
+            
+                gallery.appendChild(figureElement)
+            }
+        )
+        })
+
+
+
+
+
