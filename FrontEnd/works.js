1cc776341b54b303f1c5b6c89471d955a8cab0e3
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-14 10:30:21 +0200

    Update works.js

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,63 +1,78 @@
-//recuperation des donnees des travaux via l'API
+//recuperation des donnees des travaux via l'API//
 
 fetch("http://localhost:5678/api/works")
     .then(response => response.json())
-    .then(data => displayGallery(data)) 
-    function displayGallery(data){
-        console.log(data)
+    .then(data => displayGallery(data))
+function displayGallery(data) {
+    console.log(data)
 
-        let gallery = document.querySelector(".gallery");
-        data.forEach(
-            item => {
-                let figureElement = document.createElement("figure");
+    let gallery = document.querySelector(".gallery");
+    data.forEach(
+        item => {
+            let figureElement = document.createElement("figure");
 
-                //creating image element
-                const imgElement = document.createElement("img");
-                imgElement.src = item.imageUrl;
+            //creating image element//
+            const imgElement = document.createElement("img");
+            imgElement.src = item.imageUrl;
 
-                //adding created image to figure element
-                figureElement.appendChild(imgElement);
+            //adding created image to figure element//
+            figureElement.appendChild(imgElement);
 
-                //creating caption
-                const figcaptionElement = document.createElement("h3");
-                figcaptionElement.innerText = item.title;
+            //creating caption//
+            const figcaptionElement = document.createElement("h3");
+            figcaptionElement.innerText = item.title;
 
-                //adding caption to figure
-                figureElement.appendChild(figcaptionElement)
+            //adding caption to figure//
+            figureElement.appendChild(figcaptionElement)
 
-                // adding created figures to gallery
-                gallery.appendChild(figureElement)
-            })
-    }
+            // adding created figures to gallery//
+            gallery.appendChild(figureElement)
+        })
+}
 
-//recuperation des differentes Categories via API
+//recuperation des differentes Categories via API//
 fetch("http://localhost:5678/api/categories")
     .then(response => response.json())
     .then(categories => displaycategories(categories))
-    function displaycategories(categories){
-        console.log(categories)
+function displaycategories(categories) {
+    console.log(categories)
 
-        //filter buttons
-        filters = document.createElement("div");
-        button = document.createElement("button");
-        filters.classList.add("button");
-        anybutton = document.createElement("button");
-        button.classlist.add("filter-button");
-        anyButton.textContent = "Tous";
-        filters.appendChild(anybutton);
+    let categoriesElement = document.querySelector(".filters");
+    categories.forEach(
+        category => {
+            categories.add(category.name);
+            categories.add(category.ID);
 
-        let categoriesElement = document.querySelector(".filters");
-        categories.forEach(
-            category => {
-                button = document.createElement("button");
-                button.textContent = category;
-                div.innerText = category.name;
-                div.dataset.id = category.id;
+            //filter buttons//
+            filters = document.createElement("div");
+            filters.classlist.add("button");
+            gallery.parentNode.insertBefore(filters, gallery);
 
-        //adding categories to div containing buttons
-                categoriesElement.appendChild(div);
-                button.addEventListener("click", () => {
-                    filter-button(category);
-                })
+            // Creating Tous filter button//
+            const button = document.createElement("button");
+            button.classlist.add("filter-button");
+            button.textcontent = "Tous";
+            filters.appendChild(button);
+
+            // Adding event listener//
+            button.addEventListener("click", () => {
+                (gallery.innerHTML = ""), data();
+
+                //adding to DOM//
+                filters.appendChild(button);
             })
+        })
+
+    //gallery filtering//
+    function galleryfiltering(category) {
+
+        const gallery = document.querySelectorAll(".gallery");
+
+        gallery.forEach((item) => {
+            const Category = category.getAttribute("category");
+
+        })
     }
+}
+
+
