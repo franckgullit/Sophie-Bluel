7ddb5e117b1a630a418c7dcc59199f123304fab8
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-20 01:17:47 +0200

    Update login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,34 @@
-const response = await fetch("http://localhost:5678/api/users/login",{
-method: "POST",
-headers: { "content-type": "application/json" },
-body: JSON.stringify({ email,password})
-});
-const data = await response.json();
+function addingsubmitlistener() {
+    const loginform = document.querySelector(".login-form");
+    loginform.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Creating object for login details//
+        const logindata = {
+            email: event.target.querySelector("emailinput").value,
+            password: event.target.querySelector("passwordinput").value,
+            errormessage: event.target.getElementbyId("error-message").value,
+        };
+
+        //converting logindata to format json//
+        const submittedform = JSON.stringify(logindata);
+
+        //calling on fetch function//
+        try {
+            fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: { "content-type": "application/Json" },
+                body: "submittedform"
+            });
+
+            if (submittedform.match) {
+                const data = response.json()
+                    .then(data => localStorage.setItem);
+                location.href = "index.html";
+            } else {
+                errorMessage.textContent = "Email ou mot de passe incorrect";
+                errorMessage.style.display = "block";
+            }
+        }
+    });
+}
