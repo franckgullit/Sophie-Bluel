2cfffa294caac8f90a701c3debdd875575b59d2a
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-19 18:18:27 +0200

    Update login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,1 +1,6 @@
-
+const response = await fetch("http://localhost:5678/api/users/login",{
+method: "POST",
+headers: { "content-type": "application/json" },
+body: JSON.stringify({ email,password})
+});
+const data = await response.json();
