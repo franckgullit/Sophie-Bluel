e69ac8b50293bbdc41603ffb79b1a580dbaafb5f
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-24 11:40:00 +0200

    Update modal.js

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,1 +1,25 @@
+let modal = null
+
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"))
+// to nullify style = display:none; that i'll later set in htmml
+    target.style.display = null
+    target.removeAttribute("aria-hidden")
+    target.setAttribute("aria-modal", "true")
+    modal = target
+    modal.addEventListener("click", closeModal)
+    modal.querySelector(".close-button").addEventListener("click", closeModal)
+}
+
+const closeModal = function(e) {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute("aria-hidden", "true")
+    modal.removeAttribute("aria-modal")
+    modal.removeEventListener("click", closeModal)
+    modal.querySelector(".close-button").removeEventListener("click", closeModal)
+    modal = null
+}
 
