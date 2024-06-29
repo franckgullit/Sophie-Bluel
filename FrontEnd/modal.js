abbfb4ed64a40065d5bea78dd91792c72c5c6219
franckgullit <167395707+franckgullit@users.noreply.github.com>
2024-06-29 18:52:27 +0200

    Update modal.js

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,31 +1,41 @@
-let modal = null
+let modal = null;
 
 const openModal = function (e) {
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
-// to nullify style = display:none; in htmml//
-    target.style.display = null
-    target.removeAttribute("aria-hidden")
-    target.setAttribute("aria-modal", "true")
-    modal = target
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute("href"));
+    
+    // To nullify style = display:none; in HTML
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
 
-    //closing modal window//
-    modal.addEventListener("click", closeModal)
-    modal.querySelector(".close-button").addEventListener("click", closeModal)
-}
+    // Closing modal window
+    modal.addEventListener("click", closeModal);
+    const closeButton = modal.querySelector(".close-button");
+    if (closeButton) {
+        closeButton.addEventListener("click", closeModal);
+    }
+};
 
 const closeModal = function(e) {
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = "none"
-    modal.setAttribute("aria-hidden", "true")
-    modal.removeAttribute("aria-modal")
-    modal.removeEventListener("click", closeModal)
-    modal.querySelector(".close-button").removeEventListener("click", closeModal)
-    modal = null
-}
+    if (modal === null) return;
+    e.preventDefault();
 
-// Opening modal window when 'modifier'link is clicked//
-document.querySelectorAll("js-modal").forEach(a => {
-    a.addEventListener("click", openModal)
-})
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    
+    const closeButton = modal.querySelector(".close-button");
+    if (closeButton) {
+        closeButton.removeEventListener("click", closeModal);
+    }
+    
+    modal = null;
+};
+
+// Opening modal window when 'js-modal' link is clicked
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal);
+});
